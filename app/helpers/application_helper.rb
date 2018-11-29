2719eadd9393ab425fffa87ff034bc2c9a0ca28b
Jayesh Mistry <jayesh.mistry@optimalworkshop.com>
2018-11-29 21:49:59 +1300

    Add helper method to generate default page title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@
 
 # Some application helper!?
 module ApplicationHelper
+  # Returns the full title on a per-page basis
+  def page_title(title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "#{title} | #{base_title}" if title.present?
+    base_title
+  end
 end
